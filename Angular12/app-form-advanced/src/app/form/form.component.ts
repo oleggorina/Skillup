e9c9a40c487dab6c49d210ba558297f199166f19
oleggorina <oleg.gorina@gmail.com>
2021-11-19 15:17:38 +0200

    validation in component

--- Angular12/app-form-advanced/src/app/form/form.component.ts
+++ Angular12/app-form-advanced/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { User } from '../user.class';
 
 @Component({
@@ -6,18 +7,54 @@ import { User } from '../user.class';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
 
-  roles: string[] = ['Гость', 'Модератор', 'Пользователь']
+  roles: string[] = ['Гость', 'Модератор', 'Администратор']
   model: User = new User(1,'','', null)
+  
+  formErrors: any = {
+    name: '',
+    age: ''
+  }
+
+  validationMessages: any = {
+    name: {
+      required: 'Имя обязательно',
+      minlength: 'Имя должно содержать минимум 4 символа'
+    },
+    age: {
+      required: 'Возраст обязателен'
+    }
+  }
+
+  @ViewChild('userForm') userForm: NgForm | null = null
   
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.userForm?.valueChanges?.subscribe(() => this.onValueChanged())
   }
 
-  get diagnostic(): string {
-    return JSON.stringify(this.model)
+  onSubmit(): void {
+    console.log('Form submitted')
+  }
+
+  private onValueChanged(): void {
+    const form: any = this.userForm?.form
+    for (const field in this.formErrors) {
+      this.formErrors[field] = ''
+      const control = form.get(field)
+      if (control && control.dirty && control.invalid) {
+        const message = this.validationMessages[field]
+
+        for (const key in control.errors) {
+          console.log(message[key])
+          this.formErrors[field] += message[key] + ' '
+        }
+      } 
+    }
   }
 
 }

3fadad413fafff68ae8d91824dd71e48e0bac805
oleggorina <oleg.gorina@gmail.com>
2021-11-21 16:28:56 +0200

    updated get request

--- Angular12/app-requests/src/app/requests/requests.component.ts
+++ Angular12/app-requests/src/app/requests/requests.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 
@@ -10,7 +10,7 @@ import { Item } from '../item';
 export class RequestsComponent implements OnInit {
 
   items!: Item[]
-  item: Item = new Item(99, 'New')
+  item: Item = new Item(0, '')
   itemsUrl = '/items'
   
   constructor(private http:HttpClient) { }
@@ -20,16 +20,34 @@ export class RequestsComponent implements OnInit {
   }
 
   getData() {
-    this.http.get<Item[]>(this.itemsUrl).subscribe(
+    const url = this.itemsUrl
+    const headers = new HttpHeaders({'Content-Type': 'application/json'})
+    let params = new HttpParams()
+
+    if (this.item.id) {
+      params = params.set('id', `${this.item.id}`)
+    }
+    if (this.item.name) {
+      params = params.set('name', `${this.item.name}`)
+    }
+
+    const options = {headers, params}
+
+    this.http.get<Item[]>(url, options).subscribe(
       result => this.items = result,
       error => console.log(error.statusText)
       )
   }
 
   postData() {
+    const url = this.itemsUrl
+    const data = this.item
+    const headers = new HttpHeaders({'Content-Type': 'application/json'})
+    const options = {headers}
+
     if (!this.checkInputs(this.item)) return
     
-    this.http.post<Item>(this.itemsUrl, this.item).subscribe(
+    this.http.post<Item>(url, data, options).subscribe(
       result => this.items.push(result),
       error => console.log(error))
   }
